f430434d5c43c1557dff1841d861299d8849a1d9
rizMehdi <ccmeall@gmail.com>
2022-07-21 22:20:18 +0200

    fix df

--- map.py
+++ map.py
@@ -52,10 +52,8 @@ lon=[
 #creating a sample data consisting different points 
 # df = pd.DataFrame(np.random.randn(800, 2) / [50, 50] + [46.34, -108.7],columns=['latitude', 'longitude'])
 # df = pd.DataFrame(np.random.randn(800, 2) / [50, 50] + [46.34, -108.7] , columns=['latitude', 'longitude'])
-df = {'latitude': lat,
-                  'longitude': lon}
 
- 
+df = pd.DataFrame({'latitude': lat,'longitude': lon} , columns=['latitude', 'longitude']) 
 st.write(df)
 
 
